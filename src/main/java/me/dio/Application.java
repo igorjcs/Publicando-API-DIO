4a45ccbefa6f37423cf88acd761eec6d0ef8c557
Igor Santana <igor.j.santana@outlook.com.br>
2024-07-28 16:54:15 -0300

    Projeto finalizado

--- src/main/java/me/dio/Application.java
+++ src/main/java/me/dio/Application.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class Application {
-
+ 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
--- src/main/java/me/dio/controller/exception/GlobalExceptionHandler.java
+++ src/main/java/me/dio/controller/exception/GlobalExceptionHandler.java
@@ -1,5 +1,5 @@
 package me.dio.controller.exception;
-
+ 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
